d7a7af4a0480fed53830abdc3fda7ac1afbe128e
gdrffg <gdrffg@gmail.com>
2023-07-21 17:55:49 +0900

    FEAT: taskList 상단 날짜, 기능 바

--- src/components/home/TaskList/TaskList.styled.tsx
+++ src/components/home/TaskList/TaskList.styled.tsx
@@ -8,4 +8,102 @@ export const TaskListContainer = styled.section`
   background-color: white;
   border-radius: 24px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  //chan
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding-top: 20px;
+  overflow: scroll;
+  font-family: 'Orbit', sans-serif;
+`;
+
+/* 
+chan's mention
+width 92%
+*/
+
+// 현재 년/월/일 => 후에는 현재 선태된 날짜가 되어야 함
+export const Header = styled.header`
+  width: 92%;
+  font-size: 2.8vw;
+  margin-bottom: 5px;
+`;
+
+// 상단 2번 째 위치하는 기능 Bar
+export const FunctionBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 92%;
+  height: 5%;
+  padding-left: 10px;
+  padding-right: 15px;
+  border-radius: 10px;
+  background-color: #c2d9fa;
+  color: #8c98ae;
+`;
+
+// "테스크 추가하기" 안내말
+export const InfoWord = styled.div`
+  width: 27%;
+  height: 57%;
+  font-size: 1vw;
+  overflow: hidden;
+  span {
+    position: relative;
+    bottom: 1px;
+  }
+`;
+
+// 캘린더 표시, 테스크 생성 버튼들의 div
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 30%;
+  height: 65%;
+`;
+
+// 캘린더 표시 버튼
+export const MarkAtCalender = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 40%;
+  background-color: #c9c9c9;
+  color: whitesmoke;
+  border: none;
+  border-radius: 8px;
+  padding: 4px;
+  font-size: 0.5vw;
+  overflow: hidden;
+  &:hover {
+    background-color: skyblue;
+    color: blue;
+  }
+`;
+
+// 테스크 생성 모달 버튼
+export const CreateTask = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  color: #c2d9fa;
+  height: 60%;
+  width: 15%;
+  padding-top: 1.3px;
+  border-radius: 8px;
+  font-size: 1vw;
+  &:hover {
+    background-color: skyblue;
+    color: blue;
+  }
+  span {
+    position: relative;
+    bottom: 1px;
+  }
 `;
--- src/components/home/TaskList/TaskList.tsx
+++ src/components/home/TaskList/TaskList.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import * as T from './TaskList.styled';
 
-const TaskList = () => {
-  return <T.TaskListContainer>Task List</T.TaskListContainer>;
+const TaskList: React.FC = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  // Dnd 컨텍스트가 최상단에 있어야함
+
+  const handleCalendar = () => {
+    console.log('Mark At Calendar');
+  };
+  const handleTask = () => {
+    console.log('Create Task');
+  };
+
+  // 폰트 orbit 적용 / UI 반응형 완벽하게.
+  return (
+    <T.TaskListContainer>
+      <T.Header>{`${year}년 ${month}월 ${day}일`} </T.Header>
+      <T.FunctionBar>
+        <T.InfoWord>
+          <span>Task 추가하기</span>
+        </T.InfoWord>
+        <T.Buttons>
+          <T.MarkAtCalender onClick={handleCalendar}>
+            <span>캘린더 표시</span>
+          </T.MarkAtCalender>
+          <T.CreateTask onClick={handleTask}>
+            <span>✚</span>
+          </T.CreateTask>
+        </T.Buttons>
+      </T.FunctionBar>
+    </T.TaskListContainer>
+  );
 };
 
 export default TaskList;
