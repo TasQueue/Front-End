f40beba03bf411a84644b458d2a4e33d4f93b34e
jieun <najee0610@naver.com>
2023-07-23 21:58:55 +0900

    feat : 유저 프로필 UI 구현 - 사진 추가, 스타일링

--- src/components/home/ProfileAndNavigation/UserProfile/UserProfile.styled.tsx
+++ src/components/home/ProfileAndNavigation/UserProfile/UserProfile.styled.tsx
@@ -7,4 +7,29 @@ export const UserProfileContainer = styled.section`
   background-color: white;
   border-radius: 24px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+`;
+
+export const UserProfileName = styled.div`
+  font-size: 1.5rem;
+  font-weight: 1000;
+  background-image: linear-gradient(transparent 50%, rgba(177, 187, 231, 0.5) 50%);
+  background-repeat: no-repeat;
+`;
+export const UserProfileStatus = styled.div`
+  font-size: 1.3rem;
+  background-color: #ffffff;
+  border-radius: 20%;
+  width: 6.8vw;
+  height: 10vh;
+  opacity: 1;
+  padding: 1vh 0;
+`;
+export const UserProfileImg = styled.img`
+  width: 90%;
+  border-radius: 50%;
+  margin: 1.5vh 0 1.8vh 0;
 `;
