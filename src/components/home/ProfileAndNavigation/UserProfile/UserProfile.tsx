763c53665d7cf6a7ea65cdaff5f953ea4e5be46f
jieun <najee0610@naver.com>
2023-07-23 23:57:41 +0900

    fix : goodCat 이미지 크기 조절 문제 수정

--- src/components/home/ProfileAndNavigation/UserProfile/UserProfile.tsx
+++ src/components/home/ProfileAndNavigation/UserProfile/UserProfile.tsx
@@ -4,7 +4,7 @@ import * as U from './UserProfile.styled';
 const UserProfile = () => {
   return (
     <U.UserProfileContainer>
-      <U.UserProfileImg src='/assets/images/perfectCat.svg' alt='cat' />
+      <U.UserProfileImg src='/assets/images/goodCat.svg' alt='cat' />
       <U.UserProfileName>냐옹냥폼미쳤다</U.UserProfileName>
       <U.UserProfileStatus>
         하기 싫어도 해라 우울해도 해라 행복해도 해라 불안해도 해라 죽고 싶어도 해라
