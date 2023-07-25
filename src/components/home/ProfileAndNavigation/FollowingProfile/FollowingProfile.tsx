ac8c2b8503bb98ca952524488eca233d282500b6
jieun <najee0610@naver.com>
2023-07-25 22:05:10 +0900

    feat : 팔로잉하는 친구 목록 구현 - 배열순회 방식으로 전환

--- src/components/home/ProfileAndNavigation/FollowingProfile/FollowingProfile.tsx
+++ src/components/home/ProfileAndNavigation/FollowingProfile/FollowingProfile.tsx
@@ -1,38 +1,28 @@
 import React from 'react';
 
 import * as F from './FollowingProfile.styled';
+import ArrowButton from '../../../common/Button/ArrowButton';
 
 const FollowingProfile = () => {
+  const FriendItem = [
+    { id: 1, name: 'holy', imgSrc: '/assets/images/normalCat.svg' },
+    { id: 2, name: '냐옹냥 ', imgSrc: '/assets/images/normalCat.svg' },
+    { id: 3, name: '집', imgSrc: '/assets/images/normalCat.svg' },
+    { id: 4, name: 'realGang', imgSrc: '/assets/images/normalCat.svg' },
+    { id: 5, name: '꽁영', imgSrc: '/assets/images/normalCat.svg' },
+    { id: 6, name: '울랄라세션', imgSrc: '/assets/images/normalCat.svg' },
+  ];
   return (
     <F.FollowingProfileContainer>
       <F.FollowingProfileWrapParent>
-        <F.FollowingFriendsWrap>
-          <F.FollowingFriendsImg src='/assets/images/normalCat.svg' alt='cat' />
-          <F.FriendsName>친구1</F.FriendsName>
-        </F.FollowingFriendsWrap>
-        <F.FollowingFriendsWrap>
-          <F.FollowingFriendsImg src='/assets/images/normalCat.svg' alt='cat' />
-          <F.FriendsName>친구2</F.FriendsName>
-        </F.FollowingFriendsWrap>
-        <F.FollowingFriendsWrap>
-          <F.FollowingFriendsImg src='/assets/images/normalCat.svg' alt='cat' />
-          <F.FriendsName>친구3</F.FriendsName>
-        </F.FollowingFriendsWrap>
-        <F.FollowingFriendsWrap>
-          <F.FollowingFriendsImg src='/assets/images/normalCat.svg' alt='cat' />
-          <F.FriendsName>친구4</F.FriendsName>
-        </F.FollowingFriendsWrap>
-        <F.FollowingFriendsWrap>
-          <F.FollowingFriendsImg src='/assets/images/normalCat.svg' alt='cat' />
-          <F.FriendsName>친구5</F.FriendsName>
-        </F.FollowingFriendsWrap>
-        <F.FollowingFriendsWrap>
-          <F.FollowingFriendsImg src='/assets/images/normalCat.svg' alt='cat' />
-          <F.FriendsName>친구6</F.FriendsName>
-        </F.FollowingFriendsWrap>
+        {FriendItem.map((friend) => (
+          <F.FollowingFriendsWrap key={friend.id}>
+            <F.FollowingFriendsImg src={friend.imgSrc} alt={friend.name} />
+            <F.FriendsName> {friend.name}</F.FriendsName>
+          </F.FollowingFriendsWrap>
+        ))}
       </F.FollowingProfileWrapParent>
     </F.FollowingProfileContainer>
   );
 };
-
 export default FollowingProfile;
