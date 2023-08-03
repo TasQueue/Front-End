1fff1e4b591267671266413fd88e08fc31d4eb00
jieun <najee0610@naver.com>
2023-08-03 22:58:29 +0900

    feat : 설정페이지 - 기본 UI 틀 작성

--- src/pages/setting/SettingPage.styled.tsx
+++ src/pages/setting/SettingPage.styled.tsx
@@ -1,15 +1,101 @@
 import styled from 'styled-components';
 
-export const ChangeBtn = styled.button`
-  background-color: blue;
-`;
-
-export const GrassChangeLabel = styled.p``;
-export const ChangeWrap = styled.div``;
 export const SettingPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: 2rem;
+  background-color: #c2d9fa;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const ChangeWhiteBox = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
+  height: 50%;
+  border-radius: 5%;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  position: relative;
+  justify-content: space-between;
+`;
+export const ProfileImg = styled.img`
+  position: absolute;
+  left: 50%;
+  bottom: 80%;
+  transform: translateX(-50%);
+  border-radius: 50%;
+`;
+
+export const ChangeWrap = styled.div`
+  display: flex;
+`;
+export const NameChange = styled.input`
+  font-size: 2.2rem;
+  margin: 13vh 0 0 0;
+  font-weight: 500;
+  border: none;
+  outline: none;
+  box-shadow: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+`;
+export const StatusChange = styled.textarea`
+  font-size: 1.8rem;
+  margin: 0 0 3vh 0;
+
+  border: none;
+  outline: none;
+  box-shadow: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+`;
+
+export const GrassChangeLabel = styled.p``;
+
+export const ChangeBtn = styled.button`
+  background-color: #d9d9d9;
+  width: 9vw;
+  height: 4.8vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 16px;
+  margin: 0 0 5vh 0;
+`;
+
+export const TwoButtonWrap = styled.div`
+  display: flex;
+  height: 10%;
+  padding: 12vh 0 0 0;
+`;
+
+export const LogoutBtn = styled.button`
+  background-color: #508bff;
+  color: #ffffff;
+  width: 9vw;
+  height: 4.8vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 16px;
+  margin: 0 2vw 0 0;
+`;
+export const DeleteBtn = styled.button`
+  background-color: #ff5050;
+  color: #ffffff;
+  width: 9vw;
+  height: 4.8vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 16px;
 `;
--- src/pages/setting/SettingPage.tsx
+++ src/pages/setting/SettingPage.tsx
@@ -8,19 +8,25 @@ const SettingPage = () => {
 
   return (
     <S.SettingPageContainer>
-      <S.ChangeWrap>
-        <div>닐리리맘보</div>
-        <NameChangePopup isOpen={isOpen} setIsOpen={setIsOpen} name={name} setName={setName} />
-      </S.ChangeWrap>
-      <S.ChangeWrap>
-        <div>상태메세지입니다.</div>
-      </S.ChangeWrap>
-      <S.ChangeWrap>
-        <S.GrassChangeLabel>잔디 색</S.GrassChangeLabel>
-      </S.ChangeWrap>
-      <S.ChangeBtn onClick={() => setIsOpen(true)}>변경하기</S.ChangeBtn>
-      <button type='submit'>로그아웃</button>
-      <button type='submit'>계정삭제</button>
+      <S.ChangeWhiteBox>
+        <S.ProfileImg src='/assets/images/Cat/badCat.svg' alt='profile' />
+        <S.ChangeWrap>
+          <S.NameChange type='text' value={name} />
+          <NameChangePopup isOpen={isOpen} setIsOpen={setIsOpen} name={name} setName={setName} />
+        </S.ChangeWrap>
+        <S.ChangeWrap>
+          <S.StatusChange value={name} />
+        </S.ChangeWrap>
+        <S.ChangeWrap>
+          <S.GrassChangeLabel>잔디 색</S.GrassChangeLabel>
+          <div>color library</div>
+        </S.ChangeWrap>
+        <S.ChangeBtn onClick={() => setIsOpen(true)}>변경하기</S.ChangeBtn>
+      </S.ChangeWhiteBox>
+      <S.TwoButtonWrap>
+        <S.LogoutBtn type='submit'>로그아웃</S.LogoutBtn>
+        <S.DeleteBtn type='submit'>계정삭제</S.DeleteBtn>
+      </S.TwoButtonWrap>
     </S.SettingPageContainer>
   );
 };
