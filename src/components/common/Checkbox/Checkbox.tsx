0f33650370b97afb9b8d9208a63575a395a2ac89
dooli1917039 <dooli9819@gmail.com>
2023-08-07 02:02:59 +0900

    feat: 체크박스에 checked 인자 추가

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { CheckboxWrapper, StyledInput } from './Checkbox.styled';
 
-export const Checkbox = ({ label, updatefn }) => {
+// TODO Checkbox 쓰는 모든 곳에서 checked 값을 내려주면 default값 삭제할 것
+export const Checkbox = ({ label, updatefn, checked = false }) => {
   return (
     <CheckboxWrapper className='checkbox-wrapper'>
       <div>
         <StyledInput
           id='checkbox'
           type='checkbox'
+          defaultChecked={checked}
           onClick={(e) => {
             updatefn((prev) => !prev);
             e.stopPropagation();
