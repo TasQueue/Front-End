42de6f72c5063f4a8172859e019ca12d342b3d3a
dooli1917039 <dooli9819@gmail.com>
2023-08-07 02:06:18 +0900

    feat: recoil로 task모달 상태 관리 - 날짜/시간/반복 선택 제외

--- src/components/home/TaskModal/TaskMainModal.tsx
+++ src/components/home/TaskModal/TaskMainModal.tsx
@@ -1,44 +1,64 @@
 import CloseIcon from '@mui/icons-material/Close';
-import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import * as T from './TaskMainModal.styled';
 import { Checkbox } from '../../common/Checkbox/Checkbox';
 import CategorySelect from './CategorySelect/CategorySelect';
 import BasicTextButton from '../../common/Button/BasicTextButton';
 import TimeSelect from './TimeSelect/TimeSelect';
+import { TaskModalState } from '../../../recoil/task/atoms';
 
 interface TaskMainModalProps {
   onClose: () => void;
   controlStep: () => void;
 }
 const TaskMainModal = ({ onClose, controlStep }: TaskMainModalProps) => {
-  // TODO: 체크박스 - 기본값은 props로 받아오거나 recoil로 가져와야 함
-  const [isCompleteTask, setIsCompleteTask] = useState(false);
-  const [isAllDayTask, setIsAllDayTask] = useState(false);
-  const [isShowInCalendar, setIsShowInCalendar] = useState(false);
+  const [tempData, setTempData] = useRecoilState(TaskModalState);
 
   return (
     <>
       <T.TaskCheckbox>
-        <Checkbox label='' updatefn={setIsCompleteTask} />
+        <Checkbox
+          label=''
+          updatefn={() => {
+            setTempData({ ...tempData, isCompleted: !tempData.isCompleted });
+          }}
+          checked={tempData.isCompleted}
+        />
       </T.TaskCheckbox>
-      {/* TODO: defaultValue 나중에 수정 가능하게 함수 연결 */}
-      <T.TaskNameInput type='text' defaultValue='운동하기' />
+      <T.TaskNameInput
+        type='text'
+        value={tempData.text}
+        onChange={(e) => setTempData({ ...tempData, text: e.target.value })}
+      />
       <CloseIcon sx={{ width: '30px', height: '30px', placeSelf: 'center' }} onClick={onClose} />
       <T.TaskModalContent>
         <T.TaskCategoryAndDate>
           <CategorySelect />
+          {/* TODO 날짜 선택 기능 구현하기 */}
           <BasicTextButton
             buttonText='2023.7.14'
             buttonColor='black'
             onClick={() => alert('TODO: 날짜 선택 기능 만들기')}
           />
         </T.TaskCategoryAndDate>
-        {!isAllDayTask && <TimeSelect />}
+        {!tempData.isAllDayTask && <TimeSelect />}
         <T.CheckBoxesWrapper>
-          <Checkbox label='하루종일' updatefn={setIsAllDayTask} />
-          <Checkbox label='캘린더 표시' updatefn={setIsShowInCalendar} />
+          <Checkbox
+            label='하루종일'
+            updatefn={() => {
+              setTempData({ ...tempData, isAllDayTask: !tempData.isAllDayTask });
+            }}
+            checked={tempData.isAllDayTask}
+          />
+          <Checkbox
+            label='캘린더 표시'
+            updatefn={() => {
+              setTempData({ ...tempData, isOnCalendar: !tempData.isOnCalendar });
+            }}
+            checked={tempData.isOnCalendar}
+          />
         </T.CheckBoxesWrapper>
-        <BasicTextButton buttonText='반복 없음' buttonColor='black' onClick={() => controlStep()} />
+        <BasicTextButton buttonText='반복 없음' buttonColor='black' onClick={controlStep} />
         <T.TaskModalButton>
           <BasicTextButton
             buttonText='적용하기'
--- src/components/home/TaskModal/TaskModal.tsx
+++ src/components/home/TaskModal/TaskModal.tsx
@@ -1,10 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import * as T from './TaskModal.styled';
 import TaskMainModal from './TaskMainModal';
 import SelectRepeatModal from './SelectRepeatModal';
+import { AlltoDoState, selectedDateState } from '../../../recoil/test/atoms';
+import { TaskModalState } from '../../../recoil/task/atoms';
 
-const TaskModal = ({ onClose }) => {
+const TaskModal = ({ onClose, id }) => {
   const [isTaskMainModal, setIsTaskMainModal] = useState(true);
+  const value = useRecoilValue(AlltoDoState);
+  const originDateValue = useRecoilValue(selectedDateState);
+  const [tempData, setTempData] = useRecoilState(TaskModalState);
+
+  useEffect(() => {
+    const dateKeyString = originDateValue.toDateString();
+    const originTaskValue = value[dateKeyString].filter((task) => task.id === id)[0];
+    /**
+     * TODO 현재 다른 곳에서 state를 다 관리하지 않아서 testData 필요. 나중엔 그대로 setTempData(originTaskValue)로 수정할 것
+     */
+    const testData = {
+      ...tempData,
+      ...originTaskValue,
+    };
+    setTempData(testData);
+  }, [id]);
 
   return (
     <T.TaskModalWrapper>
