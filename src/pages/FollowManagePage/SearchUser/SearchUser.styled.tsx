c29b6ddcfc9a2ca0391863313d8ca86c1ef3e159
jieun <najee0610@naver.com>
2023-08-12 19:00:55 +0900

    feat : 목업데이터를 이용한 사용자 검색 기능 구현

--- src/pages/FollowManagePage/SearchUser/SearchUser.styled.tsx
+++ src/pages/FollowManagePage/SearchUser/SearchUser.styled.tsx
@@ -1,1 +1,29 @@
 import styled from 'styled-components';
+
+export const Label = styled.p`
+  font-size: 3rem;
+`;
+export const SearchWrap = styled.div`
+  width: 24vw;
+  height: 5vh;
+  background-color: #c2d9fa;
+`;
+export const SearchInput = styled.input`
+  font-size: 2rem;
+  background-color: transparent;
+  border: none;
+  outline: none;
+
+  :focus {
+    outline: none;
+  }
+`;
+export const SearchBtn = styled.button`
+  width: 2vw;
+  height: 3.6vh;
+  background-color: #ffffff;
+  border-radius: 50%;
+`;
+export const BtnIcon = styled.img`
+  width: 1.5vw;
+`;
--- src/pages/FollowManagePage/SearchUser/SearchUser.tsx
+++ src/pages/FollowManagePage/SearchUser/SearchUser.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import { useState } from 'react';
+import * as S from './SearchUser.styled';
+
+// 사용자 정보 타입 정의
+interface UserInfo {
+  name: string;
+  cat: string;
+}
+
+// 목업 사용자 데이터
+const mockUsers: UserInfo[] = [
+  { name: '홍길동', cat: 'one' },
+  { name: '이순신', cat: 'two' },
+  { name: '김유신', cat: 'three' },
+];
+
+const getImagePath = (cat: string) => {
+  switch (cat) {
+    case 'one':
+      return '/assets/images/Cat/badCat.svg';
+    case 'two':
+      return '/assets/images/Cat/normalCat.svg';
+    case 'three':
+      return '/assets/images/Cat/goodCat.svg';
+    case 'four':
+      return '/assets/images/Cat/perfectCat.svg';
+    default:
+      return '/assets/images/Cat/normalCat.svg';
+  }
+};
 
 const SearchUser = () => {
-  return <div>사용자 찾기 컴포넌트입니다.</div>;
+  const [searchTerm, setSearchTerm] = useState(''); // 입력하는 검색어 저장 상태
+  const [foundUsers, setFoundUsers] = useState<UserInfo[]>([]); // 검색된 사용자 정보 저장 상태
+
+  const userSearch = () => {
+    const filteredUsers = mockUsers.filter((user) => user.name.includes(searchTerm));
+    if (filteredUsers.length > 0) {
+      setFoundUsers(filteredUsers);
+    } else {
+      alert('사용자를 찾을 수 없습니다.');
+      setFoundUsers([]);
+    }
+  };
+
+  return (
+    <div>
+      <S.Label>사용자 찾기.</S.Label>
+      <S.SearchWrap>
+        <S.SearchInput
+          type='text'
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder='사용자 이름을 검색하세요.'
+        />
+        <S.SearchBtn onClick={userSearch}>
+          <S.BtnIcon src='/assets/icons/Search.svg' alt='Search' />
+        </S.SearchBtn>
+      </S.SearchWrap>
+      {foundUsers.map((user) => (
+        <div key={user.name}>
+          <p>{user.name}</p>
+          <img src={getImagePath(user.cat)} alt={`${user.name}의 이미지`} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SearchUser;
