13d141a3e96808260a4ec7ff3d72620a17d63d83
jieun <najee0610@naver.com>
2023-08-13 15:47:14 +0900

    Design: 반응형으로 전환

--- src/pages/FollowManagePage/SearchUser/SearchUser.styled.tsx
+++ src/pages/FollowManagePage/SearchUser/SearchUser.styled.tsx
@@ -3,6 +3,19 @@ import styled from 'styled-components';
 export const Label = styled.p`
   font-size: 3rem;
   margin: 2vh 0 1vh 1vw;
+  transition: font-size 0.5s ease;
+
+  @media (max-width: 1024px) {
+    font-size: 2.5rem;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.5rem;
+  }
 `;
 export const SearchWrap = styled.div`
   display: flex;
@@ -14,6 +27,20 @@ export const SearchWrap = styled.div`
   background-color: #c2d9fa;
   border-radius: 8px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  transition: height 0.5s ease;
+
+  @media (max-width: 1024px) {
+    height: 4vh;
+  }
+
+  @media (max-width: 768px) {
+    height: 3vh;
+  }
+
+  @media (max-width: 480px) {
+    height: 2vh;
+  }
 `;
 export const SearchInput = styled.input`
   display: flex;
@@ -25,6 +52,19 @@ export const SearchInput = styled.input`
   border: none;
   outline: none;
   margin: 0 1vw 0 0;
+  transition: font-size 0.5s ease;
+
+  @media (max-width: 1024px) {
+    font-size: 1.8rem;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.2rem;
+  }
 
   :focus {
     outline: none;
@@ -39,6 +79,20 @@ export const SearchBtn = styled.button`
   background-color: #ffffff;
   border-radius: 50%;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  transition: height 0.5s ease;
+
+  @media (max-width: 1024px) {
+    height: 2.5vh;
+  }
+
+  @media (max-width: 768px) {
+    height: 2vh;
+  }
+
+  @media (max-width: 480px) {
+    height: 1.8vh;
+  }
 `;
 export const BtnIcon = styled.img`
   width: 1.5vw;
@@ -58,4 +112,18 @@ export const foundUsersName = styled.p`
   font-size: 2rem;
   width: 16vw;
   margin: 0 0 0 2vw;
+
+  transition: font-size 0.5s ease;
+
+  @media (max-width: 1024px) {
+    font-size: 1.8rem;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.2rem;
+  }
 `;
