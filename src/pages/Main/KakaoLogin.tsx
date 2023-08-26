36a4d24788f42c872cd6243a4629df4b8e67647a
dooli1917039 <dooli9819@gmail.com>
2023-08-26 21:53:38 +0900

    feat: 카카오 로그인 이후 access token 세팅

--- src/pages/Main/KakaoLogin.tsx
+++ src/pages/Main/KakaoLogin.tsx
@@ -1,22 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useSetRecoilState } from 'recoil';
+import * as process from 'process';
+import { UserAccessToken } from '../../recoil/user/atom';
 
 const KakaoLogin = () => {
+  const API_URL = process.env.REACT_APP_TASQUEUE_API_URL;
   const navigate = useNavigate();
-
-  const code = new URL(window.location.href).searchParams.get('code');
-  console.log(code);
+  const code = new URL(window.location.href).searchParams.get('code'); // 카카오 인가코드
+  const setAccessToken = useSetRecoilState(UserAccessToken);
 
   useEffect(() => {
-    // axios.post(`백엔드 주소`).then((r) => {
-    //   console.log(r.data);
-    //
-    //   // 토큰을 받아서 localStorage같은 곳에 저장하는 코드를 여기에 쓴다.
-    //   localStorage.setItem('name', r.data.user_name); // 일단 이름만 저장했다.
-    //
-    //   navigate('/');
-    // });
+    // 백엔드로 인가코드를 보낸다.
+    axios.post(`${API_URL}/oauth/get`, { code }).then((res) => {
+      console.log(res.headers.authorization);
+      setAccessToken({
+        accessToken: res.headers.authorization,
+      });
+      navigate('/home');
+    });
   }, []);
 
   return <div>로그인 중입니다.</div>;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginButton from '../../components/Main/Login/LoginButton';
+
+const Main = () => {
+  return (
+    <>
+      <div>여기에 태스큐 소개??</div>
+      <LoginButton />
+    </>
+  );
+};
+
+export default Main;
