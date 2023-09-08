2c1120e5ee39f85e7e8fdf9e0532f48fb9badda7
dooli1917039 <dooli9819@gmail.com>
2023-09-08 10:07:22 +0900

    feat: /tasks/{taskId}로 태스크 단건 조회 api 생성

--- src/components/home/TaskModal/TaskModal.tsx
+++ src/components/home/TaskModal/TaskModal.tsx
@@ -5,11 +5,15 @@ import TaskMainModal from './TaskMainModal/TaskMainModal';
 import SelectRepeatModal from './SelectRepeatModal/SelectRepeatModal';
 import { AlltoDoState } from '../../../recoil/test/atoms';
 import { TaskModalState } from '../../../recoil/task/atoms';
+import { useIndividualTask } from '../../../hooks/queries/useIndividualTaskInfo';
 
 const TaskModal = ({ onClose, taskInfo }) => {
   const [isTaskMainModal, setIsTaskMainModal] = useState(true);
   const value = useRecoilValue(AlltoDoState);
   const [tempData, setTempData] = useRecoilState(TaskModalState);
+
+  // const { data } = useIndividualTask(taskId); //TODO taskInfo가 아니라 taskId만 넘겨받기
+  const { data, isLoading, isError, isSuccess } = useIndividualTask(11);
 
   useEffect(() => {
     const { dateKeyString } = taskInfo;
@@ -24,6 +28,20 @@ const TaskModal = ({ onClose, taskInfo }) => {
     };
     setTempData(testData);
   }, []);
+
+  // // 아래 로직에서 데이터 taskModal에 맞게 수정하기
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     console.log(data);
+  //     console.log('#$#');
+  //     // 데이터 세팅하기
+  //   }
+  //   if (isError) {
+  //     console.log('태스크 데이터 불러오기 실패');
+  //     onClose();
+  //   }
+  // }, [data]);
+
   return (
     <T.TaskModalWrapper>
       {isTaskMainModal ? (
